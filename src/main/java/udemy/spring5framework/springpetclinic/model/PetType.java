cd29ad0d79536f533124bfc6711cf6656bf43a0b
harshit0013 <sharma.harshit2999@yahoo.com>
2021-05-06 00:29:05 +0530

    Adding POJOs. Closes #2.

---
+++ src/main/java/udemy/spring5framework/springpetclinic/model/PetType.java
@@ -0,0 +1,13 @@
+package udemy.spring5framework.springpetclinic.model;
+
+public class PetType {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
