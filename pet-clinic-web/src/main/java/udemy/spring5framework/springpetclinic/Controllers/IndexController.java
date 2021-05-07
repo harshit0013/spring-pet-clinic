20e0eba700af7be12378bfb6fb504d6b98045f56
harshit0013 <sharma.harshit2999@yahoo.com>
2021-05-07 19:04:02 +0530

    Added Index page and controller. Closes #8.

---
+++ pet-clinic-web/src/main/java/udemy/spring5framework/springpetclinic/Controllers/IndexController.java
@@ -0,0 +1,13 @@
+package udemy.spring5framework.springpetclinic.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping({"","/","index", "index.html"})
+    public String index()
+    {
+        return "index";
+    }
+}
