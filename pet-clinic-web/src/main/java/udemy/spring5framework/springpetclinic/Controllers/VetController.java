f35784476786da73cdf75126ce87d11a459b93a0
harshit0013 <sharma.harshit2999@yahoo.com>
2021-05-07 19:28:39 +0530

    Added Vets and Owners index pages and controllers.

---
+++ pet-clinic-web/src/main/java/udemy/spring5framework/springpetclinic/Controllers/VetController.java
@@ -0,0 +1,13 @@
+package udemy.spring5framework.springpetclinic.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets()
+    {
+        return "vets/index";
+    }
+}
