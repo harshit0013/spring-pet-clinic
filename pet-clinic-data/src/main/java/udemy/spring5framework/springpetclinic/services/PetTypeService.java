c628e9c628f570f2f9a066ccc051b9599c90477c
harshit0013 <sharma.harshit2999@yahoo.com>
2021-05-12 00:39:06 +0530

    Added PetType Map service. Closes #16.

---
+++ pet-clinic-data/src/main/java/udemy/spring5framework/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package udemy.spring5framework.springpetclinic.services;
+
+import udemy.spring5framework.springpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
