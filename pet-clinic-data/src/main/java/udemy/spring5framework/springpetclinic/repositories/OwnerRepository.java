e718e02c90a29b9263d0559eb567c3251aee6700
harshit0013 <sharma.harshit2999@yahoo.com>
2021-05-15 01:30:18 +0530

    Added spring data jpa for owners. Closes #27

--- pet-clinic-data/src/main/java/udemy/spring5framework/springpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/udemy/spring5framework/springpetclinic/repositories/OwnerRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import udemy.spring5framework.springpetclinic.model.Owner;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Owner findByLastName(String lastName);
 }
