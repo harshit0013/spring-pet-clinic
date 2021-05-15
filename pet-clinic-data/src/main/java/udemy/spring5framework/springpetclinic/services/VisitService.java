aea8f50c9298a615e7a9d50c42e54ff3e2da6e39
harshit0013 <sharma.harshit2999@yahoo.com>
2021-05-15 16:52:52 +0530

    Created Visit Map Service. Closes #33.

---
+++ pet-clinic-data/src/main/java/udemy/spring5framework/springpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package udemy.spring5framework.springpetclinic.services;
+
+import udemy.spring5framework.springpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
