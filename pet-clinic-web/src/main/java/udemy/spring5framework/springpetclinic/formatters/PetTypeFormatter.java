f433dddf441ccb5f513843395d7454d8b989ce12
harshit0013 <sharma.harshit2999@yahoo.com>
2021-05-31 22:14:26 +0530

    Added ability to add or update a Pet. Closes #43

---
+++ pet-clinic-web/src/main/java/udemy/spring5framework/springpetclinic/formatters/PetTypeFormatter.java
@@ -0,0 +1,37 @@
+package udemy.spring5framework.springpetclinic.formatters;
+
+import org.springframework.format.Formatter;
+import org.springframework.stereotype.Component;
+import udemy.spring5framework.springpetclinic.model.PetType;
+import udemy.spring5framework.springpetclinic.services.PetTypeService;
+
+import java.text.ParseException;
+import java.util.Collection;
+import java.util.Locale;
+
+@Component
+public class PetTypeFormatter implements Formatter<PetType> {
+    private final PetTypeService petTypeService;
+
+    public PetTypeFormatter(PetTypeService petTypeService) {
+        this.petTypeService = petTypeService;
+    }
+
+    @Override
+    public PetType parse(String text, Locale locale) throws ParseException {
+        Collection<PetType> findPetTypes = petTypeService.findAll();
+
+        for (PetType type : findPetTypes) {
+            if (type.getName().equals(text)) {
+                return type;
+            }
+        }
+
+        throw new ParseException("type not found: " + text, 0);
+    }
+
+    @Override
+    public String print(PetType petType, Locale locale) {
+        return petType.getName();
+    }
+}
