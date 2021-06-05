5de3e69e8ef1242ea3ecfa7377943fb1c2b54e18
harshit0013 <sharma.harshit2999@yahoo.com>
2021-06-05 17:43:05 +0530

    added vets JSON endpoint.

--- pet-clinic-web/src/main/java/udemy/spring5framework/springpetclinic/Controllers/VetController.java
+++ pet-clinic-web/src/main/java/udemy/spring5framework/springpetclinic/Controllers/VetController.java
@@ -2,8 +2,13 @@ package udemy.spring5framework.springpetclinic.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import udemy.spring5framework.springpetclinic.model.Vet;
 import udemy.spring5framework.springpetclinic.services.VetService;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -18,5 +23,10 @@ public class VetController {
     {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJSON() {
+        return vetService.findAll();
     }
 }
